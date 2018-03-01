e6b89e15b16b4be44115d17a096e770cbe7bd94c
Lambert Beekhuis <lambert@jovisst.nl>
2018-03-01 09:23:53 +0100

    Initial commit

---
+++ src/App.vue
@@ -0,0 +1,29 @@
+<template>
+  <div id="app">
+    <img width="25%" src="./assets/logo.png">
+   
+    <FabricVue/>
+  </div>
+</template>
+
+<script>
+import FabricVue from "./components/FabricVue";
+
+export default {
+  name: "App",
+  components: {
+    FabricVue
+  }
+};
+</script>
+
+<style>
+#app {
+  font-family: "Avenir", Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
---
+++ src/components/FabricVue.vue
@@ -0,0 +1,67 @@
+<template>
+  <div id="fabriccanvas">
+  {{msg}}
+  
+  <canvas id="c"></canvas>
+  <button v-on:click="addObject" class="btn btn-primary">Add</button>
+
+
+   {{msg2}}
+  </div>
+</template>
+
+<script>
+import { fabric } from "fabric/dist/fabric.js";
+
+export default {
+  name: "FabricVue",
+
+  data() {
+    return {
+      msg: "Integrate fabricjs with Vue2.js",
+      msg2: "End"
+    };
+  },
+
+  methods: {
+    addObject: function() {
+      // create a rectangle object
+      alert("Add a box");
+
+      var rect2 = new fabric.Rect({
+        left: 50,
+        top: 50,
+        fill: "green",
+        width: 40,
+        height: 20
+      });
+
+      this.canvas.add(rect2);
+    }
+  },
+
+  mounted: function() {
+    var canvas = new fabric.Canvas("c", {
+      backgroundColor: "greenyellow"
+    });
+
+    this.canvas = canvas;
+
+    // create a rectangle object
+    var rect = new fabric.Rect({
+      left: 100,
+      top: 100,
+      fill: "red",
+      width: 40,
+      height: 20
+    });
+
+    // "add" rectangle onto canvas
+    canvas.add(rect);
+  }
+};
+</script>
+
+<style>
+
+</style>
